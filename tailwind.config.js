fc8fff9e588377d44aff2e2edec82559964da41e
Suresh-Krishna-P <skarunachalam31@gmail.com>
2025-05-08 22:11:47 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  content: [
-    './src/client/**/*.{js,ts,jsx,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+       content: [
+         './src/client/**/*.{js,ts,jsx,tsx}',
+       ],
+       theme: {
+         extend: {},
+       },
+       plugins: [],
+     };
