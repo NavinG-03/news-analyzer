5b5073d14d677f9948d8c111834684996f414b1c
Suresh-Krishna-P <skarunachalam31@gmail.com>
2025-05-08 22:13:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+     import react from '@vitejs/plugin-react';
+     import path from 'path';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/client'),
-      '@/components': path.resolve(__dirname, './src/client/components'),
-      '@/services': path.resolve(__dirname, './src/client/services'),
-    },
-  },
-});
+     export default defineConfig({
+       plugins: [react()],
+       resolve: {
+         alias: {
+           '@': path.resolve(__dirname, './src/client'),
+           '@/components': path.resolve(__dirname, './src/client/components'),
+           '@/services': path.resolve(__dirname, './src/client/services'),
+         },
+       },
+     });
