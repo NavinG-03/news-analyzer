28fdf4a75cdc74d5c2b98e4e327854eba2411b00
Suresh-Krishna-P <skarunachalam31@gmail.com>
2025-05-16 17:42:01 +0530

    Update FakeNewsChecker.jsx

--- frontend/src/FakeNewsChecker.jsx
+++ frontend/src/FakeNewsChecker.jsx
@@ -14,7 +14,7 @@ function FakeNewsChecker() {
     setError(null);
 
     try {
-      const response = await fetch("http://localhost:8000/predict", {
+      const response = await fetch("https://psychic-disco-69wqw95wj67qf5xp-8000.app.github.dev/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
